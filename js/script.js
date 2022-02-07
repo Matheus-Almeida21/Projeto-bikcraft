02c02d200cb9374c43e0815c5b8d4c2728ecceed
Matheus Almeida cavallari <matheus.cavallari7@gmail.com>
2022-02-07 10:08:18 -0300

    Adicionando-interação

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// ATIVAR LINKS DO MENU
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link){
+  const url = window.location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add('ativo');
+  };
+};
+
+links.forEach(ativarLink);
+
+// ATIVAR ITENS DO ORÇAMENTO
+const parametro =new URLSearchParams(window.location.search);
+
+function ativarProduto(parametro){
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true ;
+  }
+  console.log(elemento);
+};
+
+parametro.forEach(ativarProduto);
+
+// PERGUNTAS FREQUENTES
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains('ativa');
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas);
+
+console.log(perguntas)
+
+// GALERIA DE IMAGENS
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event){
+  const img = event.currentTarget;
+  console.log(img)
+  const media = window.matchMedia('(min-width: 1000px)').matches;
+  console.log(media);
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img){
+  img.addEventListener('click', trocarImagem)
+}
+
+galeria.forEach(eventosGaleria);
+
+// ANIMAÇÂO
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
